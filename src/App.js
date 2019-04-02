b2d695ec3c8beb1e51579405c9624289c0782eb1
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-04-02 21:01:05 +0530

    Add onclick on buttons. Clicking on each button is now showing on the calculator display

--- src/App.js
+++ src/App.js
@@ -1,90 +1,69 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.value
-    };
-  }
-
-  getValue() {
-    return this.state.value;
-  }
-
   render() {
     return (
-      <div id={this.state.value} class="btn">
-        {this.state.value}
+      <div id={this.props.value} class="btn" onClick={this.props.clickListener}>
+        {this.props.value}
       </div>
     );
   }
 }
 
 class Display extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ''
-    };
-  }
-
-  updateDisplay(newValue) {
-    this.setState({
-      value: newValue
-    });
-  }
-
-  getCurrentState() {
-    return this.state.value;
-  }
-
   render() {
-    return <div class="display"> {this.state.value} </div>;
+    return <div class="display">{this.props.view}</div>;
   }
 }
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
+    this.updateDisplay = this.updateDisplay.bind(this);
+    this.childRef = React.createRef();
+
     this.state = {
-      display: props.display,
-      buttons: props.buttons
+      view: '0',
+      buttons: this.createButtons()
     };
+  }
+
+  createButtons() {
+    const buttons = [];
+    for (let i = 1; i <= 9; i++) {
+      buttons.push(<Button value={i} clickListener={this.updateDisplay} />);
+    }
+
+    buttons.push(<Button value="." clickListener={this.updateDisplay} />);
+    buttons.push(<Button value="0" clickListener={this.updateDisplay} />);
+    buttons.push(<Button value="=" clickListener={this.updateDisplay} />);
+    buttons.push(<Button value="+" clickListener={this.updateDisplay} />);
+    buttons.push(<Button value="-" clickListener={this.updateDisplay} />);
+    buttons.push(<Button value="*" clickListener={this.updateDisplay} />);
+    buttons.push(<Button value="C" clickListener={this.updateDisplay} />);
+    buttons.push(<Button value="/" clickListener={this.updateDisplay} />);
+    buttons.push(<Button value="%" clickListener={this.updateDisplay} />);
+
+    return buttons;
+  }
+
+  updateDisplay(event) {
+    let clicked = event.target.id;
+    this.setState({
+      view: clicked
+    });
   }
 
   render() {
     return (
       <div class="calculator">
-        {this.state.display}
+        <Display view={this.state.view} />
         <div class="btns">{this.state.buttons}</div>
       </div>
     );
   }
 }
 
-const buttons = [];
-buttons.push(<Button value="1" />);
-buttons.push(<Button value="2" />);
-buttons.push(<Button value="3" />);
-buttons.push(<Button value="4" />);
-buttons.push(<Button value="5" />);
-buttons.push(<Button value="6" />);
-buttons.push(<Button value="7" />);
-buttons.push(<Button value="8" />);
-buttons.push(<Button value="9" />);
-buttons.push(<Button value="0" />);
-buttons.push(<Button value="." />);
-
-buttons.push(<Button value="+" />);
-buttons.push(<Button value="-" />);
-buttons.push(<Button value="*" />);
-buttons.push(<Button value="/" />);
-buttons.push(<Button value="%" />);
-
-buttons.push(<Button value="C" />);
-buttons.push(<Button value="=" />);
-
-const App = <Calculator display={<Display />} buttons={buttons} />;
+const App = <Calculator />;
 
 export default App;
